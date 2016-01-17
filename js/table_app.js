6b38256c2508934420c57a6d1e6183fb402a3ac0
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-01-17 21:28:48 +0300

    pushik

--- js/table_app.js
+++ js/table_app.js
@@ -12,6 +12,17 @@ app.controller('customersCtrl', function($scope, $http) {
       $scope.stat = response.data.teams;
     });
 
-   
+    var  myUrl= '#tab3';
+  	$scope.test = 1;
+
+  	$scope.url = myUrl;
+  	$scope.list = [{ play: 'Arsenal - mu', schet: '74 - 0'},
+  					{ play: 'Arsenal - chlensi', schet: '174 - 0'},
+  					{ play: 'Arsenal - mancity', schet: '73 - 0'},
+  					{ play: 'Arsenal - liverpool', schet: '23 - 0'},
+  					{ play: 'Arsenal - lester', schet: '29 - 0'},
+  					{ play: 'Arsenal - FCKOPYL', schet: 'игра ещё не состоялась'}
+  					];
+  	
 
 });
